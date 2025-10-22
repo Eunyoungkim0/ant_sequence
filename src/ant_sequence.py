23e5b6d4f72a2fa198df24ac6d8aff9f7bc5a051
Eunyoung Kim <ekim46@charlotte.edu>
2025-10-22 23:22:17 +0900

    add: find_ant_sequence

--- src/ant_sequence.py
+++ src/ant_sequence.py
@@ -1,5 +1,30 @@
 def find_ant_sequence(n):
-    pass
+    if n == 1:
+        return "1"
+    elif n == 2:
+        return "11"
+
+    prev = find_ant_sequence(n-1)
+    curr = []
+    stack = []
+
+    for s in prev:
+        if not stack:
+            stack.append(s)
+        else:
+            if s == stack[-1]:
+                stack.append(s)
+            else:
+                curr.append(str(len(stack)))
+                curr.append(stack[-1])
+                stack.clear()
+                stack.append(s)
+
+    if stack:
+        curr.append(str(len(stack)))
+        curr.append(stack[-1])
+
+    return ''.join(curr)
 
 
 def find_middle_two(n):
