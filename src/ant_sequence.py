9bbc8bd1577b51c84fe233c9682adaa926446b45
Eunyoung Kim <ekim46@charlotte.edu>
2025-10-24 19:33:07 +0900

    fix: put both solution

--- src/ant_sequence.py
+++ src/ant_sequence.py
@@ -1,5 +1,35 @@
 from itertools import groupby
 
+#-------------------------------------------
+# 첫 번째 풀이 시도
+#-------------------------------------------
+def find_ant_sequence(n):
+    if n == 1:
+        return "1"
+    elif n == 2:
+        return "11"
+
+    prev = find_ant_sequence(n-1)
+    curr = []
+
+    for s in prev:
+        if curr and curr[-1] == s:
+            curr[-2] += 1
+        else:
+            curr.append(1)
+            curr.append(s)
+
+    return ''.join(map(str, curr))
+
+
+def find_middle_two_1(n):
+    answer = find_ant_sequence(n)
+    return answer[len(answer)//2-1:len(answer)//2+1]
+
+
+#-------------------------------------------
+# 두 번째 풀이 시도
+#-------------------------------------------
 def find_middle_two(n):
     s = "1"
 
