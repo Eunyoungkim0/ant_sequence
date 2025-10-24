69479ef1dd40a6d7f3f6dffacbb62e8ec3ffd3ba
Eunyoung Kim <ekim46@charlotte.edu>
2025-10-24 19:21:29 +0900

    fix: use groupby

--- src/ant_sequence.py
+++ src/ant_sequence.py
@@ -1,23 +1,13 @@
-def find_ant_sequence(n):
-    if n == 1:
-        return "1"
-    elif n == 2:
-        return "11"
-
-    prev = find_ant_sequence(n-1)
-    curr = []
-
-    for s in prev:
-        if curr and curr[-1] == s:
-            curr[-2] += 1
-        else:
-            curr.append(1)
-            curr.append(s)
-
-    return ''.join(map(str, curr))
-
+from itertools import groupby
 
 def find_middle_two(n):
-    answer = find_ant_sequence(n)
-    return answer[len(answer)//2-1:len(answer)//2+1]
+    s = "1"
 
+    for _ in range(1, n):
+        next = ""
+        for key, group in groupby(s):
+            g = list(group)
+            next += f"{len(g)}{key}"
+        s = next
+
+    return s[len(s)//2 - 1 : len(s)//2 + 1]
